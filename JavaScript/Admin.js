b8a5b974faf29dd8c0ace91a5be858eca8995ae4
Eden Sofir <edensofir2710@gmail.com>
2021-12-12 10:29:40 +0200

    created methods for admin

---
+++ JavaScript/Admin.js
@@ -0,0 +1,27 @@
+const admin = new User("Admin",0,"admin@admin.com","admin",Date.now(),Status.active);
+g_state.admin = admin ;
+
+function approve_join_request(current_user){
+
+}
+
+function suspend_user(current_user){
+
+}
+
+function delete_user(current_user) {
+
+}
+
+function restore_suspend_user(current_user) {
+
+}
+
+function send_message_to_all(){
+
+}
+
+function delete_a_post_from_user(current_user) {
+
+}
+
--- JavaScript/g_state.js
+++ JavaScript/g_state.js
@@ -1,5 +1,4 @@
 const g_state = {
-    admin : new User("Admin",0,"admin@admin.com","admin",Date.now(),Status.active), //TODO : should it be const some how?
     massages: [],
     users: [],
     posts: []
