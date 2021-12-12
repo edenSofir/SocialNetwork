d49c75c328a9feeaabc2223fcb8774e5712acead
Eden Sofir <edensofir2710@gmail.com>
2021-12-12 10:18:10 +0200

    admin crated

--- JavaScript/g_state.js
+++ JavaScript/g_state.js
@@ -1,4 +1,5 @@
 const g_state = {
+    admin : new User("Admin",0,"admin@admin.com","admin",Date.now(),Status.active), //TODO : should it be const some how?
     massages: [],
     users: [],
     posts: []
