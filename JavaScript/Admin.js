2616221e58fcc8a479d1310f936605f65699c9c4
nikolra <nikolra@mta.ac.il>
2021-12-23 17:47:50 +0200

    req function were added

--- JavaScript/Admin.js
+++ JavaScript/Admin.js
@@ -1,30 +1,41 @@
-const admin = new User("Admin",0,"admin@admin.com","admin");
-admin.status = Status.active;
-g_state.admin = admin ;
+g_state.admin = new User("Admin",0,"admin@admin.com","admin");
+g_state.admin.status = Status.active;
 
-admin.approve_join_request = function(current_user) {
 
+g_state.admin.approve_join_request = function(current_user) {
+
+    g_state.users.forEach((user, index) => {
+       if(user.email_address === current_user.email_address)
+           return false;
+    });
     current_user.status = Status.active;
     g_state.users.push(current_user);
+    return true;
 }
 
-admin.suspend_user = function(current_user) {
+g_state.admin.suspend_user = function(current_user) {
 
     current_user.status = Status.suspended;
 }
 
-admin.delete_user = function(current_user) {
+g_state.admin.delete_user = function(current_user) {
 
     current_user.status = Status.deleted;
-    //TODO: does it needs to be deleted from g_state.users? if stays need to add if in all the methods
+    g_state.users.forEach((user, index) => {
+        if(user.id === current_user.id)
+        {
+            g_state.users.splice(index, 1);
+            return;
+        }
+    });
 }
 
-admin.restore_suspend_user = function(current_user) {
+g_state.admin.restore_suspend_user = function(current_user) {
 
     current_user.status = Status.active;
 }
 
-admin.delete_a_post_from_user = function(current_user, post_id) {
+g_state.admin.delete_a_post_from_user = function(current_user, post_id) {
 
     current_user.posts.forEach((post, index) => {
             if(post.id === post_id)
@@ -35,7 +46,7 @@ admin.delete_a_post_from_user = function(current_user, post_id) {
         });
 }
 
-admin.send_message_to_all_users = function(message_to_send) {
+g_state.admin.send_message_to_all_users = function(message_to_send) {
 
     const message = new Message(message_to_send, g_state.message_id, Date.now(), admin);
     g_state.message_id += 1;
@@ -43,5 +54,10 @@ admin.send_message_to_all_users = function(message_to_send) {
     g_state.users.forEach((user) => {
         user.messages.push(message);
     });
+}
+
+g_state.admin.list_all_users = function() {
+
+    return JSON.stringify( g_state.users);
 }
 
