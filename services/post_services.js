4b08b55cb3df3c8d98a62345369edd600fa84aac
Eden Sofir <edensofir2710@gmail.com>
2021-12-25 18:40:44 +0200

    post services ready

---
+++ services/post_services.js
@@ -0,0 +1,17 @@
+const {g_state} = require("../JavaScript/g_state");
+publish_current_post = function (current_user, txt)
+{
+    current_user.publish_post(txt);
+}
+
+get_all_posts_from_users = function ()
+{
+    const all_posts =[]
+    g_state.users.forEach((user,index )=>
+        user.posts.forEach((post,index) => all_posts.push(post) ));
+}
+
+delete_current_post = function (current_user, post_id)
+{
+    current_user.delete_post(post_id);
+}
