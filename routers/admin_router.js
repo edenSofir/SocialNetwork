6689cac21edc2cb86ebbda80180a97a5c6c8ee27
Eden Sofir <edensofir2710@gmail.com>
2021-12-25 18:33:11 +0200

    router for message

---
+++ routers/admin_router.js
@@ -0,0 +1,22 @@
+const {g_state} = require("../JavaScript/g_state");
+const {
+    get_user,
+    create_new_user,
+    delete_current_user,
+    restore_user,
+    suspend_user,
+    approve_user,
+    get_all_users
+} = require('controllers/admin_controller');
+
+const admin_router = g_state.express.Router();
+
+admin_router.get('/user/(:id)', get_user);
+admin_router.post('/users', create_new_user);
+admin_router.put('/user/(:id)',delete_current_user);
+admin_router.put('/user/(:id)', restore_user);
+admin_router.put('/user/(:id)', suspend_user);
+admin_router.put('/user/(:id)', approve_user);
+admin_router.get('/users', get_all_users);
+
+module.exports = admin_router;
