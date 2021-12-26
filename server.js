70727e8ab3f8600750a32389737d25181008aaa4
Eden Sofir <edensofir2710@gmail.com>
2021-12-26 14:31:46 +0200

    register and login

--- server.js
+++ server.js
@@ -57,3 +57,33 @@ app.post('/register', async (req, res) =>
         console.log(err);
     }
 })
+
+app.post("/login", async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+
+        const user = await user.find_user_by_email(email) ;
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+                { user_id: user.id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "10h",
+                }
+            );
+            user.token = token;
+
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch (err) {
+        console.log(err);
+    }
+});
