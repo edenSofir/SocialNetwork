7291eeb2a3945239bf9e98d2f1fc02ce03b84b0b
Eden Sofir <edensofir2710@gmail.com>
2021-12-26 14:50:11 +0200

    register and login

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const data_base = require('./JavaScript/data_base');
 const app = express();
 const jwt = require('jsonwebtoken');
 const user = require('./models/User');
+const {g_state} = require("../JavaScript/g_state");
 app.use(express.urlencoded(
    {
       extended: true
@@ -21,40 +22,42 @@ app.get("/",(req, res) => {
     res.status(200).send("date read");
 });
 
-
 app.listen(2718, () => console.log(`server started on port ${2718}`));
 
 app.post('/register', async (req, res) =>
 {
     try{
-       const { full_name, email ,password, user_id} = req.body;
+        const { full_name, email ,password} = req.body;
 
-       if(!(password && email && full_name && user_id))
-       {
-           res.status(400).send("all input required. please try again");
+        if(!(password && email && full_name))
+        {
+            res.status(400).send("all input required. please try again");
 
-           const is_user_exist = await user.find_user_by_id(user_id);
+            const is_user_exist = await user.find_user_by_email(email);
 
-           if(is_user_exist)
-           {
-               return res.status(409).send("user is already exist. please preform login");
-           }
+            if(is_user_exist)
+            {
+                return res.status(409).send("user is already exist. please preform login");
+            }
 
-           const encrypted_password = await bcrypt.hash(password, 10);
+            const encrypted_password = await bcrypt.hash(password, 10);
 
-           const new_user = await user.User.constructor(full_name, user_id, email.toLowerCase(), encrypted_password);
+            const id = g_state.user_id += 1;
 
-           const token = jwt.sign(
-               {
-                   user_id: new_user.id ,email},
-                    process.env.TOKEN_KEY,
-               {
-                   expiresIn: "10min",
-               }
-           );
-           new_user.token = token;
-           res.status(201).json(new_user);
-       }
+            const new_user = await user.User.constructor(full_name, id, email.toLowerCase(), encrypted_password);
+
+            const token = jwt.sign(
+                {
+                    user_id: new_user.id ,email},
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "10min",
+                }
+            );
+            new_user.token = token;
+            g_state.users.push(new_user);
+            res.status(201).json(new_user);
+        }
     }
     catch (err)
     {
@@ -74,12 +77,11 @@ app.post("/login", async (req, res) => {
         const user = await user.find_user_by_email(email) ;
 
         if (user && (await bcrypt.compare(password, user.password))) {
-            // Create token
             const token = jwt.sign(
                 { user_id: user.id, email },
                 process.env.TOKEN_KEY,
                 {
-                    expiresIn: "10h",
+                    expiresIn: "10min",
                 }
             );
             user.token = token;
