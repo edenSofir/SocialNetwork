ba957f55d98aad9fde607b1f9c4118ea82861628
nikolra <nikolra@mta.ac.il>
2021-12-27 10:28:56 +0200

    g_state updated

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,27 +1,28 @@
 const user_services = require('../services/user_service');
 const g_state = require("../JavaScript/g_state");
+const status_codes = require("http-status-codes").StatusCodes;
 
 function delete_user_account(req, res) {
     const current_id = parseInt(req.params.id);
     if (current_id < 0) {
-        res.status(g_state.g_state.status_codes.BAD_REQUEST);
+        res.status(status_codes.BAD_REQUEST);
         res.send("the current id is out of range")
         return;
     }
-    if (current_id === 1) {
-        res.status(g_state.g_state.status_codes.FORBIDDEN);
+    if (current_id === 0) {
+        res.status(status_codes.FORBIDDEN);
         res.send("the current id is the admin user - he can not be deleted");
         return;
     }
-    const user = g_state.g_state.find_user_by_id(current_id);
+    const user = g_state.find_user_by_id(current_id);
     if (user) {
-        res.status(g_state.g_state.status_codes.NOT_FOUND);
+        res.status(status_codes.NOT_FOUND);
         res.send("there is no such user in our users array");
         return;
     }
     user_services.delete_user_account(user);
 
-    res.send(JSON.stringify(g_state.g_state.users));
+    res.send(JSON.stringify(g_state.users));
 }
 
 
