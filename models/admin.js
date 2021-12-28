f8aee3e0dffe41da6ed7b64677d7f41a1db91ee6
Eden Sofir <edensofir2710@gmail.com>
2021-12-28 08:53:20 +0200

    admin as you wish

--- models/admin.js
+++ models/admin.js
@@ -1,11 +1,11 @@
 const user = require("../models/User");
 const data_base = require("../JavaScript/data_base");
 const g_state = require('../JavaScript/g_state');
+const admin = new user.User("Admin",0,"admin@admin.com","admin");
 
 const create_admin = async function () {
     console.log(g_state.users)
     if(!g_state.users.find(user => user.full_name === 'Admin')) {
-        const admin = new user.User("Admin",0,"admin@admin.com","admin");
         admin.token = 0 ;
         admin.status = user.Status.active
         g_state.users.push(admin);
