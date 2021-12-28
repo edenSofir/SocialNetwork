0cfc334e73e57c36956943aeb9ed531a0041fd5c
nikolra <nikolra@mta.ac.il>
2021-12-28 07:57:34 +0200

    send_message_to_all works.

--- models/admin.js
+++ models/admin.js
@@ -1,13 +1,14 @@
 const user = require("../models/User");
 const data_base = require("../JavaScript/data_base");
 const g_state = require('../JavaScript/g_state');
+const admin = new user.User("Admin",0,"admin@admin.com","admin");
+admin.token = 0 ;
+admin.status = user.Status.active
 
 const create_admin = async function () {
     console.log(g_state.users)
     if(!g_state.users.find(user => user.full_name === 'Admin')) {
-        const admin = new user.User("Admin",0,"admin@admin.com","admin");
-        admin.token = 0 ;
-        admin.status = user.Status.active
+
         g_state.users.push(admin);
 
         await data_base.save_data_to_file()
